ee215113ae1639b0aaaa69ccdad491103f6bed40
WJA <wja@sandquant.com>
2022-06-13 14:28:20 +0800

    V 1.1.0

--- tradetime/__version__.py
+++ tradetime/__version__.py
@@ -1,7 +1,7 @@
 # TradeTime
 
 __module__ = 'TradeTime'
-__version__ = '1.0.6'
+__version__ = '1.1.0'
 __description__ = 'Trade With TradeTime Like DateTime'
 __author__ = 'SandQuant'
 __email__ = 'data@sandquant.com'
